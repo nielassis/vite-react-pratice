a3645e81d316f78e571bcb961c2e00893cb78fad
nielassis <nielvitorba@gmail.com>
2024-12-04 15:29:47 -0300

    chore: removing useless code sections for project development

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Header, Content, Footer } from "./components/";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <Header userName="João" />
+      <Content>
+        <h1>Titulo</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum
+          voluptatum numquam, qui adipisci harum, officia fugiat ab sequi nisi
+          laudantium id, aut vitae. Aspernatur veniam in ex voluptas quo nulla!
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </Content>
+      <Footer name="Daniel Assis" />
     </>
-  )
-}
+  );
+};
 
-export default App
+export { App };

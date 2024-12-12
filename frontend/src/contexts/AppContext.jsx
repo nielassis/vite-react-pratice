ad7803ee24136cb6d052c1188233c40e75a4ecff
nielassis <nielvitorba@gmail.com>
2024-12-12 10:28:56 -0300

    feat: adds integration and interaction with back-end to the application

--- frontend/src/contexts/AppContext.jsx
+++ frontend/src/contexts/AppContext.jsx
@@ -1,56 +1,72 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AppContext } from "./AppContextProvider";
+import api from "../services/";
 
 export const AppContextProvider = (props) => {
   const { children } = props;
 
   const [criador, setCriador] = useState("Daniel Assis");
 
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      taskName: "Tarefa 1",
-    },
-    {
-      id: 2,
-      taskName: "Tarefa 2",
-    },
-    {
-      id: 3,
-      taskName: "Tarefa 3",
-    },
-    {
-      id: 4,
-      taskName: "Tarefa 4",
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
-  const createTask = (taskName) => {
-    setTasks((currentState) => {
-      const newTask = {
-        id: currentState.length + 1,
+  const loadTasks = async () => {
+    try {
+      const { data = [] } = await api.get("/tasks");
+      setTasks([...data]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const createTask = async (taskName) => {
+    try {
+      const { data: newTask } = await api.post("/tasks", {
         taskName: taskName,
-      };
-      return [...currentState, newTask];
-    });
-  };
-
-  const removeTask = (taskId) => {
-    setTasks((currentState) => {
-      const updatedTasks = currentState.filter((tasks) => tasks.id !== taskId);
-
-      return [...updatedTasks];
-    });
-  };
-
-  const updateTask = (taskId, taskName) => {
-    setTasks((currentState) => {
-      const updatedTasks = currentState.map((task) => {
-        return task.id === taskId ? { ...task, taskName: taskName } : task;
       });
-      return [...updatedTasks];
-    });
+      setTasks((currentState) => {
+        return [...currentState, newTask];
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  const removeTask = async (taskId) => {
+    try {
+      await api.delete(`/tasks/${taskId}`);
+      setTasks((currentState) => {
+        const updatedTasks = currentState.filter(
+          (tasks) => tasks.id !== taskId
+        );
+
+        return [...updatedTasks];
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateTask = async (taskId, taskName) => {
+    try {
+      const { data: updatedTask } = await api.put(`/tasks/${taskId}`, {
+        taskName: taskName,
+      });
+      setTasks((currentState) => {
+        const updatedTasks = currentState.map((task) => {
+          return task.id === taskId
+            ? { ...task, taskName: updatedTask.taskName }
+            : task;
+        });
+        return [...updatedTasks];
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
   return (
     <AppContext.Provider
       value={{ criador, setCriador, tasks, createTask, removeTask, updateTask }}
